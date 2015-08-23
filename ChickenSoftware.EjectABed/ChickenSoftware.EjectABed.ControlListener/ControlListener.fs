fccd763c525afe382a0cf9f376098fac11ea9d70
Jamie Dixon <jamessdixon@gmail.com>
2015-08-23 09:44:21 -0400

    Added Control Listener -> which will the phone app that sends instructions to eject

---
+++ ChickenSoftware.EjectABed/ChickenSoftware.EjectABed.ControlListener/ControlListener.fs
@@ -0,0 +1,27 @@
+﻿namespace ChickenSoftware.EjectABed.ControlListener
+
+open System
+open System.Collections.Generic
+open System.Diagnostics
+open System.Linq
+open System.Net
+open System.Threading
+open Microsoft.WindowsAzure
+open Microsoft.WindowsAzure.Diagnostics
+open Microsoft.WindowsAzure.ServiceRuntime
+
+type WorkerRole() =
+    inherit RoleEntryPoint() 
+
+    let log message (kind : string) = Trace.TraceInformation(message, kind)
+
+    override wr.Run() =
+
+        log "ChickenSoftware.EjectABed.ControlListener entry point called" "Information"
+        while(true) do 
+            Thread.Sleep(10000)
+            log "Working" "Information"
+
+    override wr.OnStart() = 
+        ServicePointManager.DefaultConnectionLimit <- 12
+        base.OnStart()
