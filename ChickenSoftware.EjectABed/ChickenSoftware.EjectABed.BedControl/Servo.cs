9e0a1cf5d1ec83e71db698baa9d859873b72d785
Jamie Dixon <jamessdixon@gmail.com>
2015-09-06 10:24:00 -0400

    Added Netduino b/c I can't get the dadfruit PWM working.

--- ChickenSoftware.EjectABed/ChickenSoftware.EjectABed.BedControl/Servo.cs
+++ ChickenSoftware.EjectABed/ChickenSoftware.EjectABed.BedControl/Servo.cs
@@ -1,23 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Windows.UI.Xaml;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Controls;
+using Microsoft.WindowsAzure.Storage;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using Windows.System.Threading;
+using Windows.Foundation;
 
 namespace ChickenSoftware.EjectABed.BedControl
 {
     // Yellow - Signal
     // Black (-) Ground
     // Red (+) 5 Volts
-
     public class Servo : IBedCommand
     {
-        public void Eject()
-        {
-            throw new NotImplementedException();
-        }
+        private const int SERVO_PIN = 13;
+        private GpioPin pin;
+        private GpioPinValue pinValue;
+        ThreadPoolTimer timer = null;
+        double servoUpPulseWidth = 1250;
+        double servoDownPulseWidth = 1750;
+        double servoNeutralPulseWidth = 1500;
+        double pulseFrequency = 2000;
+        double currentPulseWidth = 0;
+        Stopwatch stopwatch = null;
 
         public bool Initialize()
+        {
+            stopwatch = Stopwatch.StartNew();
+            GpioController gpio = GpioController.GetDefault();
+            if (gpio == null)
+            {
+                pin = null;
+                return false;
+            }
+            else
+            {
+                pin = gpio.OpenPin(SERVO_PIN);
+                pin.SetDriveMode(GpioPinDriveMode.Output);
+                Windows.System.Threading.ThreadPool.RunAsync(this.MotorThread,
+                    Windows.System.Threading.WorkItemPriority.High);
+                return true;
+            }
+        }
+
+        private void MotorThread(IAsyncAction action)
+        {
+            while(true)
+            {
+                
+            }
+        }
+
+        public void Eject()
         {
             throw new NotImplementedException();
         }
---
+++ ChickenSoftware.EjectABed/ChickenSoftware.EjectABed.ServoControl/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace ChickenSoftware.EjectABed.ServoControl
+{
+    public class Program
+    {
+
+        private static PWM _servo = null;
+        private static InputPort _trigger = null;
+        private static bool _servoReady = false;
+        private const uint SERVO_UP = 1250;
+        private const uint SERVO_DOWN = 1750;
+        private const uint SERVO_NEUTRAL = 1500;
+        
+
+        public static void Main()
+        {
+            OutputPort led = new OutputPort(Pins.ONBOARD_LED, false);
+            SetUpTrigger();
+            SetUpServo();
+
+            while (true)
+            {
+                led.Write(true);
+                Thread.Sleep(250);
+                led.Write(false);
+                Thread.Sleep(250);
+            }
+        }
+
+        internal static void SetUpTrigger()
+        {
+            _trigger = new InputPort(Pins.GPIO_PIN_D10, false, Port.ResistorMode.PullDown);
+            _trigger.OnInterrupt += _trigger_OnInterrupt;
+
+        }
+
+        private static void _trigger_OnInterrupt(uint data1, uint data2, DateTime time)
+        {
+            _servoReady = false;
+            ActivateServoForBellows("UP", 20000);
+            ActivateServoForBellows("DOWN", 20000);
+            _servoReady = true;
+        }
+
+        internal static void SetUpServo()
+        {
+            uint period = 20000;
+            uint duration = SERVO_NEUTRAL;
+            _servo = new PWM(PWMChannels.PWM_PIN_D5, period, duration, PWM.ScaleFactor.Microseconds, false);
+            _servo.Start();
+            _servoReady = true;
+        }
+
+        internal static void ActivateServoForBellows(String direction, Int32 duration)
+        {
+            if(_servoReady)
+            {
+                if (direction == "UP")
+                {
+                    _servo.Duration = SERVO_UP;
+
+                }
+                else if (direction == "DOWN")
+                {
+                    _servo.Duration = SERVO_DOWN;
+
+                }
+                else
+                {
+                    _servo.Duration = SERVO_NEUTRAL;
+                }
+                Thread.Sleep(duration);
+                _servo.Duration = SERVO_NEUTRAL;
+
+            }
+        }
+    }
+}
